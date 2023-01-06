0fb4400f7391e81ccf49937b03f796f227ff414a
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-01-06 19:52:55 +0300

    Temporary loading background of the forest to a window.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,49 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	// load the forest background
+	sf::Texture backgroundTexture;
+	// TODO set path to something that works in all computers in a better way
+	if (!backgroundTexture.loadFromFile("../assets/game_background_1.png"))
+	{
+		std::cout << "error loading texture";
+		exit(-1);
+	}
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	const auto width = 1280.f;
+	const auto height = 720.f;
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+	sf::Sprite backgroundSprite;
+	backgroundSprite.setTexture(backgroundTexture);
+	backgroundSprite.setScale(width/backgroundTexture.getSize().x , height/backgroundTexture.getSize().y );
 
-    return 0;
+	//const sf::Vector2f backgroundSize(backgroundSprite.getTexture()->getSize().x * backgroundSprite.getScale().x,
+	//	backgroundSprite.getTexture()->getSize().y * backgroundSprite.getScale().y);
+
+
+
+	sf::RenderWindow window(sf::VideoMode(width, height), "Wonderland");
+	
+
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+
+		window.draw(backgroundSprite);
+		window.display();
+	}
+
+	return 0;
 }

84bdbdf372ccc447770ddcc8601fe6bc92d9a7d3
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-03-09 00:04:10 +0300

    Remove old part about manual loading.

--- src/Game.cpp
+++ src/Game.cpp
@@ -214,53 +214,6 @@ namespace wonderland {
 				m_characters.push_back(std::move(enemyCharacter));
 			}
 		}
-
-
-		// todo : 1. save todos between the lines if relevant 2. add enemy config to the json instead of manual one and remove all the manual load under here (including the commented player one)
-		//{
-		//	// first character (which the player controls)
-		//	TextureChangeInfo info;
-		//	info.scale = { 2.f, 2.f };
-		//	info.upCut = 3.f;
-		//	std::vector<Animation> playerCharacterAnim;
-		//	playerCharacterAnim.resize(animationTypeToInt(AnimationType::Count));
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::WalkingRight)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Walk_6.png", 6, 0.1f, info);
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::WalkingLeft)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Walk_6.png", 6, 0.1f, info,true);
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::AttackRight)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Attack2_6.png", 6, 0.1f, info);
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::AttackLeft)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Attack2_6.png", 6, 0.1f, info, true);
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::JumpRight)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Jump_8.png", 8, 0.1f, info);
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::JumpLeft)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Jump_8.png", 8, 0.1f, info, true);
-		//	playerCharacterAnim[animationTypeToInt(AnimationType::Idle)] = Animation("../assets/2_Owlet_Monster/Owlet_Monster_Idle_4.png", 4, 0.1f, info);
-
-		//	const int playerStrength = 25;
-		//	auto playerCharacter = std::make_shared<Character>(sf::Vector2f{ 100.f, 500.f + info.upCut * 2 }, CharacterType::Player, playerCharacterAnim, playerStrength);
-		//	m_characters.push_back(std::move(playerCharacter));
-		//}
-
-		// todo make a pointer to animation then if null don't do it later or use optional
-
-		//std::vector<Animation> enemyCharacterAnim;
-		// todo might need to leave more space for animations to not look weird
-		//{
-		//	TextureChangeInfo info;
-		//	info.leftCut = 35.f;
-		//	info.upCut = 60.f;
-		//	info.rightCut = 35.f;
-		//	info.scale = { 2.f, 2.f };
-
-		//	enemyCharacterAnim.resize(animationTypeToInt(AnimationType::Count));
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::WalkingRight)] = Animation("../assets/Skeleton_Warrior/Walk.png", 7, 0.1f, info, false);
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::WalkingLeft)] = Animation("../assets/Skeleton_Warrior/Walk.png", 7, 0.1f, info, true);
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::AttackRight)] = Animation("../assets/Skeleton_Warrior/Attack_3.png", 4, 0.1f, info, false);
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::AttackLeft)] = Animation("../assets/Skeleton_Warrior/Attack_3.png", 4, 0.1f, info, true);
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::JumpRight)] = {}; // todo lol enemies cannot jump (I think as a game design it is fine just wanna improve it some time xD)
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::JumpLeft)] = {};
-		//	enemyCharacterAnim[animationTypeToInt(AnimationType::Idle)] = Animation("../assets/Skeleton_Warrior/Idle.png", 7, 0.1f, info, true);
-
-		//	const int enemyStrength = 10;
-		//	auto enemyCharacter = std::make_shared<Bot>(sf::Vector2f{ 700.f, 309 + info.upCut * 2 }, enemyCharacterAnim, enemyStrength);
-		//	m_characters.push_back(std::move(enemyCharacter));
-		//}
 	}
 
 	void Game::handleKeyboardEvents(float dt)
